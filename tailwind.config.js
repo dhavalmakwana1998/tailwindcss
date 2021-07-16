e3f6193bc701a5d9ae7b0d47c8997e6cb14ade7b
Dhaval Makawana <dhavalmakawana191@gmail.com>
2021-07-16 19:14:32 +0530

    add configue file for customization

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dm: "#000",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
